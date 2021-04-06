35dcc4df39e3da3aa9a23a1eb7736decd456d5d1
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-06 17:20:51 +0900

    set api_level of users to be non-null

--- db/migrate/20210406074653_add_api_level_to_users.rb
+++ db/migrate/20210406074653_add_api_level_to_users.rb
@@ -1,5 +1,5 @@
 class AddApiLevelToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :api_level, :integer, default: 0
+    add_column :users, :api_level, :integer, default: 0, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20210406074653) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.integer "api_level", default: 0
+    t.integer "api_level", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

0d242d6fc6c4bd488faa3dec1aa3683e8ef31a19
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-07 18:31:59 +0900

    revised entity files - put the entities into module Entities

--- app/api/v1/entities/post.rb
+++ app/api/v1/entities/post.rb
@@ -1,8 +1,10 @@
 module V1
-  class Post < Grape::Entity
-    expose :id, documentation: { type: 'Integer', desc: '게시글 ID' }
-    expose :title, documentation: { type: 'String', desc: '게시글 제목' }
-    expose :user_name, documentation: { type: 'String', desc: '작성자 이름' }
-    expose :created_at, documentation: { type: 'DateTime', desc: '게시 시간' }
+  module Entities
+    class Post < Grape::Entity
+      expose :id, documentation: { type: 'Integer', desc: '게시글 ID' }
+      expose :title, documentation: { type: 'String', desc: '게시글 제목' }
+      expose :user_name, documentation: { type: 'String', desc: '작성자 이름' }
+      expose :created_at, documentation: { type: 'DateTime', desc: '게시 시간' }
+    end
   end
 end
--- app/api/v1/entities/user.rb
+++ app/api/v1/entities/user.rb
@@ -1,8 +1,10 @@
 module V1
-  class User < Grape::Entity
-    expose :id, documentation: { type: 'Integer', desc: '사용자 ID' }
-    expose :user_name, documentation: { type: 'String', desc: '사용자 이름' }
-    expose :email, documentation: { type: 'String', desc: '이메일' }
-    expose :last_sign_in_at, documentation: { type: 'DateTime', desc: '최근 접속 시간' }
+  module Entities
+    class User < Grape::Entity
+      expose :id, documentation: { type: 'Integer', desc: '사용자 ID' }
+      expose :user_name, documentation: { type: 'String', desc: '사용자 이름' }
+      expose :email, documentation: { type: 'String', desc: '이메일' }
+      expose :last_sign_in_at, documentation: { type: 'DateTime', desc: '최근 접속 시간' }
+    end
   end
 end

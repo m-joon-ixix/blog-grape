f680bbdb96948e75e2aa0b72be57d8733d271909
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-09 19:40:53 +0900

    made up DASHBOARD level functions for users API

--- app/api/v1/entities/user.rb
+++ app/api/v1/entities/user.rb
@@ -4,7 +4,7 @@ module V1
       expose :id, documentation: { type: 'Integer', desc: '사용자 ID' }
       expose :user_name, documentation: { type: 'String', desc: '사용자 이름' }
       expose :email, documentation: { type: 'String', desc: '이메일' }
-      expose :last_sign_in_at, documentation: { type: 'DateTime', desc: '최근 접속 시간' }
+      expose :api_level, documentation: { type: 'Integer', desc: '관리 권한 여부' }
     end
   end
 end

2aecb816890855372c8485cd81cb8f76a4b0639f
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-12 14:40:11 +0900

    post author can also delete comments on his post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,11 @@ class Comment < ApplicationRecord
   after_create :increment_num_of_comments
   before_destroy :decrement_num_of_comments
 
+  # returns the comments that can be deleted by a user with 'current_user_id'
+  scope :able_to_delete, -> (current_user_id) {
+    joins(:post).where("posts.user_id = ? OR comments.user_id = ?", current_user_id, current_user_id)
+  }
+
   def user_name
     user.user_name
   end

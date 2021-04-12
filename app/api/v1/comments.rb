bd93b60cf47b13d11060820ac8a078c5aee80675
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-12 12:16:35 +0900

    made up DASHBOARD level functions for comments API

--- app/api/v1/comments.rb
+++ app/api/v1/comments.rb
@@ -32,14 +32,17 @@ module V1
             success_response(nil, represented.as_json)
           end
 
-          desc '특정 댓글 삭제, 권한은 댓글 작성자와 해당 게시글 작성자에게만 있음'
+          desc '특정 댓글 삭제. DASHBOARD-level 사용자가 아니라면 권한은 댓글 작성자에게만 있음'
           delete do
             ids = params[:id].split(',').map(&:to_i)
             comments = Comment.where(id: ids)
             return failure_response('해당하는 댓글이 존재하지 않습니다.') if comments.empty?
 
-            comments = comments.where(user_id: current_user.id)
-            return failure_response('이 댓글들을 삭제할 권한이 없습니다.') if comments.empty?
+            # DASHBOARD-level 사용자는 어떤 댓글이든 삭제 가능
+            unless current_user.is_admin?
+              comments = comments.where(user_id: current_user.id)
+              return failure_response('이 댓글들을 삭제할 권한이 없습니다.') if comments.empty?
+            end
 
             deleted_ids = comments.pluck(:id)
             # hook manually put in for bulk-deletion

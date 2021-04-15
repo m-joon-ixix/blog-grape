ca17b218d87313e43d29a3219e0bacf28f7072d2
Minjoon Choi <minjoonchoi08@gmail.com>
2021-04-15 16:13:02 +0900

    uniqueness validation of a subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :subscribing_user, :class_name => "User"  # 구독자
   belongs_to :subscribed_user, :class_name => "User"  # 구독 받은 사람
+
+  # check the uniqueness of a subscription from one to another
+  validates :subscribing_user_id, uniqueness: { scope: :subscribed_user_id }
+
 end
